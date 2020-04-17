fc5fb1c93c4d3c268d1c344a9aef6598cf672f33
Mohamed Mounir <mohamed_mounir@mentor.com>
2020-04-17 21:29:35 +0200

    Compelete traffic simulation implementation

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -6,13 +6,21 @@
 
 /* Implementation of class "MessageQueue" */
 
-/* 
+ 
 template <typename T>
 T MessageQueue<T>::receive()
 {
     // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
     // to wait for and receive new messages and pull them from the queue using move semantics. 
-    // The received object should then be returned by the receive function. 
+    // The received object should then be returned by the receive function.
+    std::unique_lock<std::mutex> uLock(_mutex);
+    _condition.wait(uLock, [this] { return !_queue.empty(); });
+
+    // remove last vector element from queue
+    T msg = std::move(_queue.back());
+    _queue.pop_back();
+
+    return msg;
 }
 
 template <typename T>
@@ -20,8 +28,12 @@ void MessageQueue<T>::send(T &&msg)
 {
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+    std::lock_guard<std::mutex> uLock(_mutex);
+
+    _queue.push_back(std::move(msg));
+    _condition.notify_one();
 }
-*/
+
 
 /* Implementation of class "TrafficLight" */
 
@@ -36,6 +48,12 @@ void TrafficLight::waitForGreen()
     // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
     // runs and repeatedly calls the receive function on the message queue. 
     // Once it receives TrafficLightPhase::green, the method returns.
+    while(true)
+    {
+        TrafficLightPhase phase = _msgs.receive();
+        if(TrafficLightPhase::green == phase)
+            return;
+    }
 }
 
 TrafficLightPhase TrafficLight::getCurrentPhase()
@@ -57,7 +75,7 @@ void TrafficLight::cycleThroughPhases()
     // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
     // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles.
 
-    std::chrono::time_point<std::chrono::system_clock> lastUpdate;
+    std::chrono::time_point<std::chrono::system_clock> lastUpdate = std::chrono::system_clock::now();;
     double cycleDuration = rand()%3 + 4; // generate random cycle time in seconds
 
     while (true)
@@ -78,7 +96,7 @@ void TrafficLight::cycleThroughPhases()
              lastUpdate = std::chrono::system_clock::now();
             
             // send update message
-            ;
+            _msgs.send(std::move(this->_currentPhase));
         }
     }
 }
